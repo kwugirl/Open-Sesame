a036dddc38c2f723104d6901c10793e715a68613
kwu_workmac <kwugirl@gmail.com>
2013-04-18 15:23:13 -0700

    added timeout to serial connection so doesn't hang on ser.readline()

--- read_arduino.py
+++ read_arduino.py
@@ -1,9 +1,11 @@
 import serial
 
 def read_arduino():
-    ser = serial.Serial(port='/dev/tty.usbmodemfa131', baudrate=19200)
+    ser = serial.Serial(port='/dev/tty.usbmodemfa131', baudrate=19200, timeout= 3) # will time out any .readline() below after 3 seconds
 
-    print ser.readline() # to read and print out the initial connection confirmation message
+    print ser.readline() # print out connection confirmation message
+
+    status = "off"
 
     while True: # keeping reading data from Arduino
         line = ser.readline() # reading serial output from Arduino line by line
@@ -11,6 +13,7 @@ def read_arduino():
 
         if data == "start": # z button pressed down for first time
             gesture = []
+            status = "on"
             print "starting to collect data now"
 
         elif data == "stop": # z button had been pressed down previously, now just let go of it to end collection of data
@@ -20,7 +23,7 @@ def read_arduino():
 
             return gesture
 
-        else:
+        elif status == "on":
             print "collecting data..."
             vector_list = str.split((data), ",") # break apart into a 3-item [x, y, z] list
 
@@ -28,3 +31,8 @@ def read_arduino():
                 vector_list[i] = int(vector_list[i]) # convert contents of vector_list into numbers
 
             gesture.append(vector_list)
+
+        else:
+            ser.close()
+
+            return "Too slow to give input, timed out!"
